68b9a25bf394d478222d50d317878b1e7f0e835f
vesa <vesa@pop-os.localdomain>
2024-03-02 16:36:50 +0200

    add fuzzy search

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,39 @@ use ratatui::{
     symbols::border,
     widgets::{block::*, *},
 };
-use std::env;
 use std::io;
 use std::process::Command;
+use std::{cmp::min, env};
 
 #[derive(Debug, Default)]
 pub struct App {
-    counter: u8,
+    index: u32,
     searchterm: String,
-    branches: Vec<String>,
+    all_branches: Vec<String>,
+    found_branches: Vec<String>,
     exit: bool,
+    selected: bool,
 }
 
 impl App {
-    // runs the application's main loop until the user quits
-    pub fn run(&mut self, terminal: &mut tui::Tui) -> io::Result<()> {
+    pub fn new(branches: Vec<String>, searchterm: Option<String>) -> Self {
+        App {
+            index: 0,
+            searchterm: searchterm.unwrap_or_default(),
+            all_branches: branches.clone(),
+            found_branches: branches,
+            exit: false,
+            selected: false,
+        }
+    }
+
+    pub fn run(&mut self, terminal: &mut tui::Tui) -> io::Result<bool> {
         while !self.exit {
             terminal.draw(|frame| self.render_frame(frame))?;
             self.handle_events()?;
+            self.update_search();
         }
-        Ok(())
+        Ok(self.selected)
     }
 
     fn render_frame(&self, frame: &mut Frame) {
@@ -34,9 +47,10 @@ impl App {
 
     fn handle_key_event(&mut self, key_event: KeyEvent) {
         match key_event.code {
-            KeyCode::Esc => self.exit(),
-            KeyCode::Left => self.increment_counter(),
-            KeyCode::Right => self.decrement_counter(),
+            KeyCode::Esc => self.exit = true,
+            KeyCode::Enter => self.select(),
+            KeyCode::Down => self.increment_index(),
+            KeyCode::Up => self.decrement_index(),
             KeyCode::Backspace => self.delete_char(),
             KeyCode::Char(c) => self.add_char(c),
             _ => {}
@@ -55,32 +69,49 @@ impl App {
         Ok(())
     }
 
-    fn exit(&mut self) {
-        self.exit = true;
+    fn update_search(&mut self) {
+        self.found_branches.clear();
+        self.found_branches = self.all_branches.clone();
+        self.found_branches.sort_by_key(|b| {
+            wagner_fischer(&self.searchterm.chars().collect(), &b.chars().collect())
+        });
     }
 
-    fn decrement_counter(&mut self) {
-        self.counter += 1;
+    fn decrement_index(&mut self) {
+        if self.index == 0 {
+            self.index = self.found_branches.len() as u32 - 1;
+        } else {
+            self.index -= 1;
+        }
     }
 
-    fn increment_counter(&mut self) {
-        self.counter -= 1;
+    fn increment_index(&mut self) {
+        if self.index == self.found_branches.len() as u32 - 1 {
+            self.index = 0;
+        } else {
+            self.index += 1;
+        }
     }
 
     fn delete_char(&mut self) {
-        if !self.searchterm.is_empty() {
-            self.searchterm.pop();
-        }
+        self.searchterm.pop();
+        self.index = 0;
     }
 
     fn add_char(&mut self, c: char) {
         self.searchterm.push(c);
+        self.index = 0;
+    }
+
+    fn select(&mut self) {
+        self.exit = true;
+        self.selected = true;
     }
 }
 
 impl Widget for &App {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Title::from(" Counter App Tutorial ".bold());
+        let title = Title::from(" GITGRE ".bold());
         let instructions = Title::from(Line::from(vec![
             " Decrement ".into(),
             "<Left>".blue().bold(),
@@ -99,35 +130,105 @@ impl Widget for &App {
             .borders(Borders::ALL)
             .border_set(border::THICK);
 
-        let counter_text = Text::from(vec![Line::from(vec![
+        let mut visible_branches = vec![Line::from(vec![
             "Value: ".into(),
-            self.counter.to_string().yellow(),
+            self.index.to_string().yellow(),
             self.searchterm.to_string().blue(),
-        ])]);
+        ])];
 
-        Paragraph::new(counter_text)
+        self.found_branches
+            .iter()
+            .enumerate()
+            .for_each(|(ind, branch)| {
+                if ind == self.index as usize {
+                    visible_branches.push(Line::from(branch.to_string().on_red()));
+                } else {
+                    visible_branches.push(Line::from(branch.to_string()));
+                }
+            });
+        let visible_branches = Text::from(visible_branches);
+
+        Paragraph::new(visible_branches)
             .centered()
             .block(block)
             .render(area, buf);
     }
 }
 
+fn wagner_fischer(pattern: &Vec<char>, text: &Vec<char>) -> i32 {
+    let mut table: Vec<i32> = (0..=text.len() as i32).collect();
+    let mut top_left = 0;
+    for y in 1..=pattern.len() {
+        for x in 1..=text.len() {
+            let cost = if pattern[y - 1] == text[x - 1] { 0 } else { 1 };
+            let new_top_left = table[x];
+            table[x] = min(top_left + cost, min(table[x - 1] + 1, table[x]) + 1);
+            top_left = new_top_left;
+        }
+    }
+
+    table[table.len() - 1] - text.len() as i32
+}
+
+// // wagner_fischer2(pattern: &Vec<char>, text: &Vec<char>) -> u32 {
+//     let mut table: Vec<Vec<u32>> = vec![(0..=text.len() as u32).collect(); pattern.len() + 1];
+//     for i in 0..=pattern.len() {
+//         table[i][0] = i as u32;
+//     }
+//     for y in 1..=pattern.len() {
+//         for x in 1..=text.len() {
+//             let cost = if pattern[y - 1] == text[x - 1] { 0 } else { 1 };
+//             table[y][x] = min(
+//                 table[y - 1][x - 1] + cost,
+//                 min(table[y][x - 1] + 1, table[y - 1][x]) + 1,
+//             );
+//         }
+//     }
+//     // for t in &table {
+//     //     println!("{:?}", t);
+//     // }
+//     table[table.len() - 1][table[0].len() - 1]
+// }
+
 fn main() -> io::Result<()> {
-    // let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
     // println!("{:?}", args);
 
-    let output = Command::new("git branch")
-        .output()?;
-    println!("status: {}", output.status);
-    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+    let output = Command::new("/usr/bin/git").arg("branch").output()?;
+    // println!("status: {}", output.status);
+    // println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    // println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
 
+    let output_lines: Vec<String> = String::from_utf8_lossy(&output.stdout)
+        .split_terminator('\n')
+        .filter(|l| !l.starts_with('*'))
+        .map(|l| l.trim().to_string())
+        .collect();
 
-    // let mut terminal = tui::init()?;
-    // let app_result = App::default().run(&mut terminal);
-    // tui::restore()?;
-    // app_result
-    Ok(())
+    if output_lines.len() == 1 {
+        println!("You only have one branch {}", output_lines[0]);
+        return Ok(());
+    } else if output_lines.is_empty() {
+        println!("You don't have any branches");
+        return Ok(());
+    }
+
+    let mut terminal = tui::init()?;
+    let mut app = App::new(output_lines, args.get(1).cloned());
+    let app_result: Result<bool, io::Error> = app.run(&mut terminal);
+    tui::restore()?;
+    match app_result {
+        Ok(selected) => {
+            if selected {
+                Command::new("/usr/bin/git")
+                    .arg("checkout")
+                    .arg::<String>(app.found_branches[app.index as usize].clone())
+                    .output()?;
+            }
+            Ok(())
+        }
+        Err(e) => Err(e),
+    }
 }
 
 #[cfg(test)]
@@ -165,10 +266,10 @@ mod tests {
     fn handle_key_event() -> io::Result<()> {
         let mut app = App::default();
         app.handle_key_event(KeyCode::Right.into());
-        assert_eq!(app.counter, 1);
+        assert_eq!(app.index, 1);
 
         app.handle_key_event(KeyCode::Left.into());
-        assert_eq!(app.counter, 0);
+        assert_eq!(app.index, 0);
 
         let mut app = App::default();
         app.handle_key_event(KeyCode::Char('q').into());
